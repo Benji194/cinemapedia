ae6586057e0333237018d3101c53856aa4f41737
Arturo Benjamin Vazquez Muñoz <vazben94@gmail.com>
2024-01-16 17:08:14 -0600

    avance

---
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,36 @@
+
+
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+
+final movieInfoProvider =
+    StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+  final fetchMoreMovies = ref.watch(movieRespositoryProvider).getNowPlaying;
+  return MoviesNotifier(fetchMoreMovies: fetchMoreMovies);
+});
+
+
+typedef GetMovieCallback = Future <Movie> Function ( String movieId) ;
+
+class MovieMapNotifier extends StateNotifier <Map <String , Movie>> {
+
+  final GetMovieCallback getMovie ;
+  
+  MovieMapNotifier({
+    required this.getMovie
+  }
+  ) : super({});
+
+  Future<void> loadMovie( String movieId ) async{
+    if (state[movieId] != null) return ;
+
+    final movie = await  getMovie(movieId) ;
+
+    state = {...state , movieId : movie} ;
+
+  }
+
+}
