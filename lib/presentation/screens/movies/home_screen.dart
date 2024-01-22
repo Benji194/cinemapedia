c03e9b0f1d9d928bcdf7422584fe530b6cb67637
Arturo Benjamin Vazquez Muñoz <vazben94@gmail.com>
2024-01-22 15:55:25 -0600

    fin de goroute config

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,113 +1,39 @@
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:cinemapedia/presentation/providers/providers.dart';
+
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
 
-  const HomeScreen({super.key});
+  final int pageIndex ;
+
+
+  const HomeScreen({
+    super.key,
+    required this.pageIndex
+  });
+
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    SizedBox(), //<- Categorias view
+    FavoritesView()
+  ] ;
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottonNavigation(),
+    return  Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes,
+      ),
+      bottomNavigationBar: CustomBottonNavigation( 
+        currentIndex: pageIndex,
+      ),
       // bottomNavigationBar : Placeholder(
       // ),
     );
   }
 }
 
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
 
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final initialLoading = ref.watch(initialLoadingProvider);
-
-    if (initialLoading) {
-      return const FullScreenLoader();
-    }
-
-    final slideShowMovies = ref.watch(moviesSlideshowProvider);
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    if (slideShowMovies.isEmpty) {
-      return const CircularProgressIndicator();
-    }
-    return CustomScrollView(slivers: [
-      const SliverAppBar(
-        floating: true,
-        flexibleSpace: FlexibleSpaceBar(
-          title: CustomAppbar(),
-        ),
-      ),
-      SliverList(
-        delegate: SliverChildBuilderDelegate(
-          (context, index) {
-            return Column(
-              children: [
-                // const CustomAppbar(),
-                MoviesSlideShow(movies: slideShowMovies),
-                MovieHorizontalListView(
-                  movies: nowPlayingMovies,
-                  title: 'En cines',
-                  subTitle: '',
-                  loadNextPage: () {
-                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-                MovieHorizontalListView(
-                  movies: upcomingMovies,
-                  title: 'Proximamente',
-                  subTitle: 'Este mes',
-                  loadNextPage: () {
-                    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-                MovieHorizontalListView(
-                  movies: popularMovies,
-                  title: 'Populares',
-                  // subTitle: 'Este mes',
-                  loadNextPage: () {
-                    ref.read(popularMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-                MovieHorizontalListView(
-                  movies: topRatedMovies,
-                  title: 'Mejor Calificadas',
-                  subTitle: 'Desde Siempre',
-                  loadNextPage: () {
-                    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-                  },
-                ),
-                const SizedBox(
-                  height: 2,
-                ),
-              ],
-            );
-          },
-          childCount: 1,
-        ),
-      )
-    ]);
-  }
-}
