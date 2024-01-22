0c184a6f53a9639ecc4ffe9e643c6d6d34fce88a
Arturo Benjamin Vazquez Muñoz <vazben94@gmail.com>
2024-01-22 12:21:13 -0600

    go router oficial implementado

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -12,6 +12,30 @@ final appRouter = GoRouter(
         return HomeScreen(childView: child);
       },
       routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return const HomeView();
+          },
+          routes: [
+          GoRoute(
+                    path: 'movie/:id',
+                    name: MovieScreen.name,
+                    builder: (context, state) {
+                      final movieId = state.pathParameters['id'] ?? 'no-id';
+                      return MovieScreen(
+                        movieId: movieId,
+                      );
+                    }
+          )
+          ]
+        ),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        ),
         
       ]
     )
@@ -21,14 +45,14 @@ final appRouter = GoRouter(
   //     name: HomeScreen.name,
   //     builder: (context, state) => const HomeScreen( childView: HomeView(), ),
   //     routes: [
-  //       GoRoute(
-  //         path: 'movie/:id',
-  //         name: MovieScreen.name,
-  //         builder: (context, state) {
-  //           final movieId = state.pathParameters['id'] ?? 'no-id';
-  //           return MovieScreen(
-  //             movieId: movieId,
-  //           );
+        // GoRoute(
+        //   path: 'movie/:id',
+        //   name: MovieScreen.name,
+        //   builder: (context, state) {
+        //     final movieId = state.pathParameters['id'] ?? 'no-id';
+        //     return MovieScreen(
+        //       movieId: movieId,
+        //     );
   //         },
   //       ),
   //     ]),
