5c2f3fdbef478d4ea2be06e60191639987a10a64
Arturo Benjamin Vazquez Muñoz <vazben94@gmail.com>
2024-01-24 15:26:15 -0600

    movie favorite scroll

--- lib/presentation/widgets/movies/movie_masonry.dart
+++ lib/presentation/widgets/movies/movie_masonry.dart
@@ -1,49 +1,72 @@
-import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart'; 
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-class MovieMansonry extends StatefulWidget {
+import 'movie_poster_link.dart';
 
-  final List<Movie> movies ;
-  final VoidCallback ? loadNextPage ;
 
-  const MovieMansonry({
-    super.key,
-    required this.movies,
+class MovieMasonry extends StatefulWidget {
+
+  final List<Movie> movies;
+  final VoidCallback? loadNextPage;
+
+  const MovieMasonry({
+    super.key, 
+    required this.movies, 
     this.loadNextPage
-    
   });
 
   @override
-  State<MovieMansonry> createState() => _MovieMansonryState();
+  State<MovieMasonry> createState() => _MovieMasonryState();
 }
 
-class _MovieMansonryState extends State<MovieMansonry> {
+class _MovieMasonryState extends State<MovieMasonry> {
 
-  // todo Initstate
+  final scrollController = ScrollController();
 
-  // todo dispose
+  @override
+  void initState() {
+    super.initState();
+    
+    scrollController.addListener(() { 
+      if ( widget.loadNextPage == null ) return;
+
+      if ( (scrollController.position.pixels + 100) >= scrollController.position.maxScrollExtent ) {
+        widget.loadNextPage!();
+      }
+
+    });
+
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric( horizontal: 10 ),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
       child: MasonryGridView.count(
-        crossAxisCount: 3,
+        controller: scrollController,
+        crossAxisCount: 3, 
         mainAxisSpacing: 10,
         crossAxisSpacing: 10,
         itemCount: widget.movies.length,
         itemBuilder: (context, index) {
-          if (index ==1) {
+
+          if ( index == 1 ) {
             return Column(
               children: [
-                const SizedBox(height: 20,),
-                MoviePosterLink( movie : widget.movies[index] ) ,
+                const SizedBox(height: 40 ),
+                MoviePosterLink( movie: widget.movies[index] )
               ],
             );
           }
-          return MoviePosterLink( movie : widget.movies[index] ) ;
+          
+          return MoviePosterLink( movie: widget.movies[index] );
         },
       ),
     );
